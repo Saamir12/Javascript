71dc7407f0c422075fcac177bcc7247760e98473
Saamir <saamirkhan7513@gmail.com>
2025-01-15 20:39:55 +0530

    a

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let random= Math.random();
-let a = prompt("Please enter first operand")
+let a = prompt(" Enter first operand")
 let c = prompt("Enter operator")
-let b = prompt("Please enter second operand")
+let b = prompt(" Enter second operand")
 
 
 let obj = {
@@ -12,6 +12,7 @@ let obj = {
 
 }
 
+// does faulty calculations
 if(random<0.1){
 switch(c){
 case("+"): console.log("-");
@@ -26,7 +27,7 @@ break;
 console.log(`${a} ${c} ${b}`)
 alert(`${eval(`${a} ${c} ${b}`)}`);
 
-
+// does correct calculations
  if(random>0.1){
 console.log(`${a} ${c} ${b}`)
      alert(`${eval(`${a} ${c} ${b}`)}`);
